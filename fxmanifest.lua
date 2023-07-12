5d992ea0263e5b402732d90b524e59067c77694e
Berkay Ediz İnce <37938768+berkayediz@users.noreply.github.com>
2023-07-12 17:16:14 -0500

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,7 +2,7 @@ fx_version 'cerulean'
 game 'gta5'
 
 author 'Erenoske, Berkayediz'
-description 'EE Simple Show ID Script'
+description 'EE Simple Chat Clear Script'
 
 server_scripts {
     'server.lua',
